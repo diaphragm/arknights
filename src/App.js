2f78a89ac20259d46f1a3055a253c823a871ebfc
diaphragm <diaphragm@9kv.org>
2020-10-11 19:27:15 +0900

    reactalize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './style/App.css'
+import Operators from './data/operators.json'
+import Tags from './data/tags.json'
+import Title from './components/Title'
+import TagList from './components/TagList'
+import Result from './components/Result'
 
-function App() {
+const App = () => {
+  const operators = Operators.operators
+  const tags = Tags.tags
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Title />
+      <TagList tags={tags} />
+      <Result operators={operators} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../style/Title.css'
+
+const Title = (props) => {
+  return (
+    <div className="title"><span>アークナイツ</span> <span>公開求人検索</span></div>
+  )
+}
+
+export default Title
