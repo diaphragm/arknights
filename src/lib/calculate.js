e0f20c3806df2084cc890c19c5ef0f520f21aab0
diaphragm <7163746+diaphragm@users.noreply.github.com>
2021-01-10 21:11:07 +0900

    微リファクタ

--- src/lib/calculate.js
+++ src/lib/calculate.js
@@ -1,28 +1,28 @@
+// https://tech-blog.s-yoshiki.com/2019/06/1342/
 export const combination = (nums, k) => {
-  let ans = [];
+  let ans = []
   if (nums.length < k) {
     return []
   }
   if (k === 1) {
     for (let i = 0; i < nums.length; i++) {
-      ans[i] = [nums[i]];
+      ans[i] = [nums[i]]
     }
   } else {
     for (let i = 0; i < nums.length - k + 1; i++) {
-      let row = combination(nums.slice(i + 1), k - 1);
+      let row = combination(nums.slice(i + 1), k - 1)
       for (let j = 0; j < row.length; j++) {
-        ans.push([nums[i]].concat(row[j]));
+        ans.push([nums[i]].concat(row[j]))
       }
     }
   }
-  return ans;
+  return ans
 }
 
 export const findCharacter = (tags, charData) => {
   return charData.filter(char => {
-    if (char.rarity === 6 && !tags.includes('TOP')) {
-      return false
-    }
+    // 上級エリートタグ無しの場合は★6を除外
+    if (char.rarity === 6 && !tags.includes('TOP')) return false
 
     return tags.every(tag => char.tags.includes(tag))
   })
--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,10 +1,18 @@
 const CHARACTER_TABLE_URL = 'https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/ja_JP/gamedata/excel/character_table.json'
 const GACHA_TABLE_URL = 'https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/ja_JP/gamedata/excel/gacha_table.json'
 
+const fetchCharacterTable = async () => {
+  return await (await fetch(CHARACTER_TABLE_URL)).json()
+}
+
+const fetchGachaTable = async () => {
+  return await (await fetch(GACHA_TABLE_URL)).json()
+}
+
 export const fetchCharData = async () => {
   const [characterTable, gachaTable] = await Promise.all([
-    (async () => await (await fetch(CHARACTER_TABLE_URL)).json())(),
-    (async () => await (await fetch(GACHA_TABLE_URL)).json())()
+    fetchCharacterTable(),
+    fetchGachaTable()
   ])
 
   const availableCharNameList = gachaTable.recruitDetail
@@ -16,13 +24,14 @@ export const fetchCharData = async () => {
   console.log(availableCharNameList)
 
   return Object.entries(characterTable)
-    .filter(([key, char]) => availableCharNameList.includes(char.name))
+    .filter(([, char]) => availableCharNameList.includes(char.name))
     .map(([key, char]) => {
       const tags = char.tagList || []
       tags.push(char.position, char.profession)
-      if (char.rarity + 1 === 1) { tags.push('ROBOT') }
-      if (char.rarity + 1 === 5) { tags.push('SENIOR') }
-      if (char.rarity + 1 === 6) { tags.push('TOP') }
+      if (char.rarity + 1 === 1) tags.push('ROBOT')
+      if (char.rarity + 1 === 5) tags.push('SENIOR')
+      if (char.rarity + 1 === 6) tags.push('TOP')
+
       return { key, name: char.name, rarity: char.rarity + 1, tags }
     })
 }
